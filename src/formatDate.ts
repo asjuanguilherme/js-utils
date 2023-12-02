0ec5a2fb01d0658e9df4ef45b22da3c27b951fbc
Juan Guilherme <asjuanguilherme@gmail.com>
2023-12-02 07:46:24 -0300

    chore: :wrench: configure husky + lint-staged and prettier

--- src/formatDate.ts
+++ src/formatDate.ts
@@ -1,6 +1,6 @@
 export const formatDate = (date: Date | string) =>
-  new Intl.DateTimeFormat("pt-BR", {
-    dateStyle: "full",
-    timeStyle: "short",
-    timeZone: "America/Sao_Paulo",
-  }).format(new Date(date));
+  new Intl.DateTimeFormat('pt-BR', {
+    dateStyle: 'full',
+    timeStyle: 'short',
+    timeZone: 'America/Sao_Paulo',
+  }).format(new Date(date))
--- src/formatPrice.ts
+++ src/formatPrice.ts
@@ -1,7 +1,7 @@
 export const formatPrice = (value: number) => {
-  return value.toLocaleString("pt-BR", {
+  return value.toLocaleString('pt-BR', {
     minimumFractionDigits: 2,
-    style: "currency",
-    currency: "BRL",
-  });
-};
+    style: 'currency',
+    currency: 'BRL',
+  })
+}
