65b08725e535d5de017af83ca697780eef72b9c6
Juan Guilherme <asjuanguilherme@gmail.com>
2023-12-02 06:48:58 -0300

    build: :wrench: configure rollup build

--- generateIndexFile.js
+++ generateIndexFile.js
@@ -1,18 +1,19 @@
-const fs = require("fs");
-const path = require("path");
-const glob = require("glob");
+const fs = require('fs')
+const path = require('path')
+const glob = require('glob')
 
-function generateIndexFile(directory) {
-  const indexPath = path.join(directory, "index.ts");
-  const files = glob.sync("**/*.ts", { cwd: directory });
+function generateIndexFile() {
+  const indexPath = path.join('./src', 'index.ts')
+  const files = glob.sync('**/*.ts', { cwd: './src' })
 
   const content = files
-    .map((file) => `export * from './${file.split(".")[0]}'`)
-    .join("\n");
+    .filter(i => i !== 'index.ts')
+    .map(file => `export * from './${file.split('.')[0]}'`)
+    .join('\n')
 
-  fs.writeFileSync(indexPath, content);
+  fs.writeFileSync(indexPath, content)
 
-  console.log(`Generated ${indexPath}`);
+  console.log(`Generated ${indexPath}`)
 }
 
-generateIndexFile("./src/");
+module.exports = generateIndexFile
---
+++ rollup.config.mjs
@@ -0,0 +1,43 @@
+import peerDepsExternal from 'rollup-plugin-peer-deps-external'
+import nodeResolve from '@rollup/plugin-node-resolve'
+import commonJs from '@rollup/plugin-commonjs'
+import babel from '@rollup/plugin-babel'
+import typescript from '@rollup/plugin-typescript'
+import { terser } from 'rollup-plugin-terser'
+
+import { createRequire } from 'node:module'
+const require = createRequire(import.meta.url)
+const packageJson = require('./package.json')
+const generateIndexFile = require('./generateIndexFile.js')
+
+generateIndexFile()
+
+/** @type {import('rollup').RollupOptions} */
+export default [
+  {
+    input: 'src/index.ts',
+    output: [
+      {
+        file: packageJson.main,
+        format: 'cjs',
+        sourcemap: true,
+        name: 'asjuanguilherme-js-utils',
+      },
+      {
+        file: packageJson.module,
+        format: 'esm',
+        sourcemap: true,
+      },
+    ],
+    plugins: [
+      peerDepsExternal(),
+      typescript({ tsconfig: './tsconfig.json' }),
+      nodeResolve({
+        extensions: ['.ts', '.tsx'],
+      }),
+      babel(),
+      commonJs(),
+      terser(),
+    ],
+  },
+]
